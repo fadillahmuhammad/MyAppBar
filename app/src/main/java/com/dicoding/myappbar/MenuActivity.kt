5eff0103fab2965b0cb5c6e736678fcf38cb3d15
fadillahmuhammad <fadillahmuhammad1102@gmail.com>
2023-09-23 09:45:48 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/myappbar/MenuActivity.kt
@@ -0,0 +1,29 @@
+package com.dicoding.myappbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.dicoding.myappbar.databinding.ActivityMenuBinding
+
+class MenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                .setOnEditorActionListener { textView, actionId, event ->
+                    searchBar.text = searchView.text
+                    searchView.hide()
+                    Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
+        }
+    }
+}
